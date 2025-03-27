5373e81e2a44d657178146866c3fc253519c7371
donadomini <najacob@hanmail.net>
2025-03-27 22:34:07 +0900

    solagratia

---
+++ 알림사항.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Soli Deo Gloria !</title>
+
+  <style type="text/css">
+    a { text-decoration:none }
+  </style>
+
+  <style>
+  p {
+
+    color:red;
+    text-align: center;
+    font-size:25px;
+    font-family:Courier New!
+  }
+
+  /* .box {
+      -ms-overflow-style: none; /* IE and Edge */
+    }
+  .box::-webkit-scrollbar {
+      display: none; /* Chrome, Safari, Opera*/
+     } */
+
+ </style>
+
+</head>
+
+<body>
+
+      <p>[알림사항]<br></p>
+
+      <div style="width: 100%; height: 100%; background: #d3d3d3; marginwidth: 100px; marginheight: 10px; scrolling="auto";">
+           <br>
+           <ol>
+
+             <li><font size=2 color=black>본 사이트의 번역물의 번역의 완전함은 독자분들의 몫입니다.<br></font></li>
+
+             <li><font size=2 color=black>벨직신앙고백서와 스코트랜드 신앙고백서와 프랑스 신앙고백서의 원천은 'Creeds of Christendom, Vol III, Philip Schaff, Baker Books' 입니다.<br></font></li>
+
+           </ol>
+           <br>
+      </div>
+
+
+</body>
+</html>
